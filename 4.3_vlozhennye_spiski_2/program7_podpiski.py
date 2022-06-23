b0be90dae37748c74aeddc90e172f749224ba693
Arystan Muzdakhanov <amuzdakhanov@rambler.ru>
2022-06-23 14:25:31 +0600

    modified 4.3

--- 4.3_vlozhennye_spiski_2/program7_podpiski.py
+++ 4.3_vlozhennye_spiski_2/program7_podpiski.py
@@ -1,6 +1,23 @@
+"""
+s = input().split()
+n = int(input())
+tmp = []
+res = []
+for i in range(len(s)):
+    tmp.append(s[i])
+    if len(tmp) == n:
+        res.append(tmp)
+        tmp = []
+if len(s) % n != 0:
+    res.append(tmp)
+print(res)
+"""
+
 s = input().split()
 l_s = len(s)
+first = [[]]
+res = []
 for i in range(1,l_s+1):
-    for j in range(0,l_s):
-
-    j -= 1
+    for j in range(0,l_s+1-i):
+        res.append(s[j:j+i])
+print(first+res)
