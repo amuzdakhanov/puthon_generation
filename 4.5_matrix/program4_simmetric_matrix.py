472a5572f3733d757fb33fffda7859ac27f6b98d
Arystan Muzdakhanov <amuzdakhanov@rambler.ru>
2022-06-27 17:42:07 +0600

    4.5 modified

--- 4.5_matrix/program4_simmetric_matrix.py
+++ 4.5_matrix/program4_simmetric_matrix.py
@@ -4,8 +4,11 @@ for i in range(n):
     temp = [int(num) for num in input().split()]
     matrix.append(temp)
 
-print(matrix)
-
+flag = 'YES'
+counter = 0
 for i in range(n):
     for j in range(n):
-
+        if matrix[i][j] != matrix[j][i] :
+            flag = 'NO'
+            break
+print(flag)
---
+++ 4.5_matrix/program7_povorot_matricy.py
@@ -0,0 +1,6 @@
+n = int(input())
+matrix = []
+for i in range(n):
+    temp = [int(num) for num in input().split()]
+    matrix.append(temp)
+
