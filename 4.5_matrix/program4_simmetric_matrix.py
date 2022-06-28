e7675f9810be6db5bd08b243c9907fb665e92037
Arystan Muzdakhanov <amuzdakhanov@rambler.ru>
2022-06-28 17:30:57 +0600

    4.5 modified

--- 4.5_matrix/program4_simmetric_matrix.py
+++ 4.5_matrix/program4_simmetric_matrix.py
@@ -12,3 +12,4 @@ for i in range(n):
             flag = 'NO'
             break
 print(flag)
+
--- 4.5_matrix/program7_povorot_matricy.py
+++ 4.5_matrix/program7_povorot_matricy.py
@@ -4,3 +4,9 @@ for i in range(n):
     temp = [int(num) for num in input().split()]
     matrix.append(temp)
 
+text = matrix[::-1]
+
+for i in range(n):
+    for j in range(n):
+        print(text[j][i], end=' ')
+    print()
